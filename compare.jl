0a6c6730e23eee22516a941b4419ad8ff4a884ea
Ciarán O'Mara <Ciaran.OMara@utas.edu.au>
2019-11-21 21:02:49 +1100

    Visual separators

--- compare.jl
+++ compare.jl
@@ -15,24 +15,28 @@ results_v1 = load(joinpath(dirname(@__FILE__), "benchmark_GenomicFeatures_v1", "
 results_v2 = load(joinpath(dirname(@__FILE__), "benchmark_GenomicFeatures_v2", "results.bson"))
 
 # Quick print outs of merged results.
+@info "intervals"
 for (name, result) in [results_v1["intervals"] |> collect; results_v2["intervals"] |> collect] |> (xs) -> sort(xs, by=first)
     print(name, " ")
     display(result)
     println()
 end
 
+@info "collection/push"
 for (name, result) in [results_v1["collection"]["push"] |> collect; results_v2["collection"]["push"] |> collect] |> (xs) -> sort(xs, by=first)
     print(name, " ")
     display(result)
     println()
 end
 
+@info "collection/bulk_insertion"
 for (name, result) in [results_v1["collection"]["bulk_insertion"] |> collect; results_v2["collection"]["bulk_insertion"] |> collect] |> (xs) -> sort(xs, by=first)
     print(name, " ")
     display(result)
     println()
 end
 
+@info "collection/eachoverlap"
 for (name, result) in [results_v1["collection"]["eachoverlap"] |> collect; results_v2["collection"]["eachoverlap"] |> collect] |> (xs) -> sort(xs, by=first)
     print(name, " ")
     display(result)
